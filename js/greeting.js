1b3280737259639cdef28eef2720c15180bfae37
StefanWest <wst_stfn@hotmail.com>
2018-02-22 20:58:50 -0600

    created javascript added linter

---
+++ js/greeting.js
@@ -0,0 +1,13 @@
+
+var greetUser = function greetUser(){
+    var firstName = prompt( "What's your name?" );
+
+    if( !firstName ){
+        greetUser();
+    }
+    else{
+        alert( "Hello" + firstName );
+    }
+};
+
+greetUser();
